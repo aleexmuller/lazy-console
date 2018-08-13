3dd7fd1b8438e93dc629f1373cd51a7f56b7487d
Alex Muller <amuller@debian.amuller>
2018-08-12 21:46:58 -0300

    adicionando validacao de arquivo de configuracao com tratamento de erros

--- bin/index.js
+++ bin/index.js
@@ -2,13 +2,6 @@
 const program = require('commander');
 const LazyConsole = require('./../lib/lazyconsole');
 
-const dependencies = {
-  fileSystem: require('fs'),
-  process: require('child_process')
-};
-
-const LazyController = LazyConsole.getController(dependencies);
-
-program.command("run <alias> [args...]").action(LazyController.execute);
-
+program.command("run <alias> [args...]")
+  .action(LazyConsole.execute);
 program.parse(process.argv);
